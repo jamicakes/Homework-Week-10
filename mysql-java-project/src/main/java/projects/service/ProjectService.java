bcc1c3630005bac46fd8513a245510ffb56d829f
Jami <jamitvelez@gmail.com>
2023-05-24 10:23:15 -0700

    added week 10

---
+++ mysql-java-project/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,43 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+
+  private ProjectDao projectDao = new ProjectDao();
+  
+  
+  
+  
+  public Project addProject(Project project) {
+  return projectDao.insertProject(project); 
+  
+  } // AddProject class 
+
+
+
+
+  public List<Project> fetchAllProjects() {
+   return projectDao.fetchAllProjects();
+  } // fetchAllProjects 
+
+
+
+
+  public Project fetchProjectById(Integer projectId) {
+    return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException (
+        "Project with project ID=" + projectId + " does not exist."));
+  }
+            
+ //Optional<Project> op = projectDao.fetchProjectById(projectId); 
+  
+  
+  
+
+  } // Project service class 
+
+
